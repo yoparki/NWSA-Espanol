3317134e83fd075e8ac2072c6a890787552f5a1e
yoparki <yoparko@gmail.com>
2025-07-03 19:12:56 -0400

    Add files via upload

---
+++ banco.js
@@ -0,0 +1,12 @@
+const banco = [
+  {
+    "pregunta": "Telecom Tower",
+    "opciones": [
+      "Technician - I  Practice Test",
+      "1.) De acuerdo con las normas de la industria aplicables, ¿cuál es la altura",
+      "mínima de una superficie o suelo inferior donde se debe utilizar la",
+      "protección contra caídas?"
+    ],
+    "respuesta": "Technician - I  Practice Test"
+  }
+];
